cb2bba1ada82eb6d2f4ed0e3f3bf3219fafe0fe5
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2024-08-22 23:44:28 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 15848 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        per(n, m, new boolean[n], 0, 0);
+
+    }
+
+    private static void per(int n, int m, boolean[] v, int depth, int idx) {
+        if (depth == m) {
+            for (int a : arr) {
+                System.out.print(a + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = idx; i < n; i++) {
+            if (!v[i]) {
+                v[i] = true;
+                arr[depth] = i + 1;
+                per(n, m, v, depth + 1, i + 1);
+                v[i] = false;
+            }
+        }
+    }
+}
