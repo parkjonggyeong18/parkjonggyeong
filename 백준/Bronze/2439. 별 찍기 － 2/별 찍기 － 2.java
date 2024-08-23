08ef98ab942f0a62cb0848c8ea797a3e426db479
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2024-08-23 13:13:25 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 268 ms, Memory: 15484 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+import java.io.*;
+
+//조합, 멱집합 풀기(햄버거 다이어트)
+public class Main {
+
+	static char[][] map;
+	static int bomb[][], time, r, c, n;
+	static int dx[] = { 1, -1, 0, 0 };
+	static int dy[] = { 0, 0, 1, -1 };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			for(int j = 0; j <n - i - 1; j++) {
+				System.out.print(" ");
+			}
+			
+			for (int j = n; j >= n - i; j--) {
+				System.out.print("*");
+
+			}
+
+			System.out.println();
+
+		}
+	}
+}
