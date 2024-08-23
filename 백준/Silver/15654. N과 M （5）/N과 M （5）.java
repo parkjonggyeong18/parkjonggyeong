649f586598c799a86792d04deca19193dd7cbde7
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2024-08-23 09:26:04 +0900

    [Silver III] Title: N과 M (5), Time: 272 ms, Memory: 29228 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,58 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static int[] arr, gap;
+	public static int N, M;
+	public static StringBuilder sb = new StringBuilder();
+
+	static boolean v[];
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		gap = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			gap[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(gap);
+		arr = new int[M];
+		v = new boolean[N];
+		dfs(0, 0);
+		System.out.println(sb);
+	}
+
+	public static void dfs(int depth, int idx) {
+
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[i]).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if(!v[i]) {
+				v[i] = true;
+				arr[depth] = gap[i];
+				dfs(depth + 1, i);
+				v[i] = false;
+				
+			}
+
+		}
+	}
+
+}
