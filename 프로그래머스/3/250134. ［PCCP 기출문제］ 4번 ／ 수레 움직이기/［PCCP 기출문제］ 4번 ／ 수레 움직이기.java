681ab12fe276b9ec6bfaa5a4a2660477cb6f1134
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2024-10-02 22:01:09 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 3.83 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 class Solution {
     public static int N, M, Ans = Integer.MAX_VALUE;
-    public static int[][] Map, Red, Blue;
+    public static int[][] Map;
+    public static boolean[][] Red, Blue;
     public static int[] dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1};
     public static int rx, ry, bx, by;
 
@@ -15,41 +16,41 @@ class Solution {
                 int nx = x2 + dx[i];
                 int ny = y2 + dy[i];
                 if (nx < 1 || ny < 1 || nx > N || ny > M) continue;
-                if (Blue[nx][ny] == 1 || Map[nx][ny] == 3 || Map[nx][ny] == 5) continue;
-                Blue[nx][ny] = 1;
+                if (Blue[nx][ny] || Map[nx][ny] == 3 || Map[nx][ny] == 5) continue;
+                Blue[nx][ny] = true;
                 Dfs(count + 1, x1, y1, nx, ny);
-                Blue[nx][ny] = 0;
+                Blue[nx][ny] = false;
             }
         } else if (Map[x2][y2] == 4) {
             for (int i = 0; i < 4; i++) {
                 int nx = x1 + dx[i];
                 int ny = y1 + dy[i];
                 if (nx < 1 || ny < 1 || nx > N || ny > M) continue;
-                if (Red[nx][ny] == 1 || Map[nx][ny] == 4 || Map[nx][ny] == 5) continue;
-                Red[nx][ny] = 1;
+                if (Red[nx][ny] || Map[nx][ny] == 4 || Map[nx][ny] == 5) continue;
+                Red[nx][ny] = true;
                 Dfs(count + 1, nx, ny, x2, y2);
-                Red[nx][ny] = 0;
+                Red[nx][ny] = false;
             }
         } else {
             for (int i = 0; i < 4; i++) {
                 int nx = x1 + dx[i];
                 int ny = y1 + dy[i];
                 if (nx < 1 || ny < 1 || nx > N || ny > M) continue;
-                if (Red[nx][ny] == 1 || Map[nx][ny] == 5) continue;
+                if (Red[nx][ny] || Map[nx][ny] == 5) continue;
 
                 for (int j = 0; j < 4; j++) {
                     int nnx = x2 + dx[j];
                     int nny = y2 + dy[j];
                     if (nnx < 1 || nny < 1 || nnx > N || nny > M) continue;
-                    if (Blue[nnx][nny] == 1 || Map[nnx][nny] == 5) continue;
+                    if (Blue[nnx][nny] || Map[nnx][nny] == 5) continue;
                     if (nx == nnx && ny == nny) continue; 
                     if ((nx == x2 && ny == y2) && (nnx == x1 && nny == y1)) continue;
 
-                    Red[nx][ny] = 1;
-                    Blue[nnx][nny] = 1;
+                    Red[nx][ny] = true;
+                    Blue[nnx][nny] = true;
                     Dfs(count + 1, nx, ny, nnx, nny);
-                    Red[nx][ny] = 0;
-                    Blue[nnx][nny] = 0;
+                    Red[nx][ny] = false;
+                    Blue[nnx][nny] = false;
                 }
             }
         }
@@ -59,19 +60,19 @@ class Solution {
         N = maze.length;
         M = maze[0].length;
         Map = new int[N + 1][M + 1];
-        Red = new int[N + 1][M + 1];
-        Blue = new int[N + 1][M + 1];
+        Red = new boolean[N + 1][M + 1];
+        Blue = new boolean[N + 1][M + 1];
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
                 Map[i][j] = maze[i - 1][j - 1];
                 if (Map[i][j] == 1) {
-                    Red[i][j] = 1;
+                    Red[i][j] = true;
                     rx = i;
                     ry = j;
                 }
                 if (Map[i][j] == 2) {
-                    Blue[i][j] = 1; 
+                    Blue[i][j] = true; 
                     bx = i;
                     by = j;
                 }
