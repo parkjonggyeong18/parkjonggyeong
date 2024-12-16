a7f3021b8e1858a22b306a83ac12917fce6ab12c
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2024-12-16 18:15:14 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 296 ms, Memory: 48048 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static char[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new char[N][N];
+
+        run(0, 0, N, false);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    private static void run(int x, int y, int n, boolean blank) {
+        if (blank) {
+            for (int i = x; i < x + n; i++) {
+                for (int j = y; j < y + n; j++) {
+                    arr[i][j] = ' ';
+                }
+            }
+            return;
+        }
+        if (n == 1) {
+            arr[x][y] = '*';
+            return;
+        }
+
+        int size = n / 3;
+        int count = 0;
+        for (int i = x; i < x + n; i += size) {
+            for (int j = y; j < y + n; j += size) {
+                count++;
+                if (count == 5) {
+                    run(i, j, size, true);
+                } else {
+                    run(i, j, size, false);
+                }
+            }
+        }
+    }
+}
+
