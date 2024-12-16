63b4479e30b899514f359f66fd4a3bbfdfe58237
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2024-12-16 18:36:53 +0900

    [Gold IV] Title: 공유기 설치, Time: 280 ms, Memory: 29036 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int arr[], n, c;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        System.out.println(bs(c,1, arr[n - 1]));
+
+    }
+
+    private static int bs(int key, int low, int high) {
+        int mid;
+        while(low <= high){
+            mid = (low + high) / 2;
+            int cnt = 1;
+            int  p = 0;
+            for(int i = 1; i < n; i++){
+                if(arr[i] - arr[p] >= mid){
+                    p = i;
+                    cnt++;
+                }
+            }
+            if(cnt < key){
+                high = mid - 1;
+                continue;
+            }
+                low = mid + 1;
+
+        }
+        return low -1 ;
+    }
+}
+
