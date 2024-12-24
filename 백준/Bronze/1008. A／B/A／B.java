1c1defd447e2fb54b82d46a4826f70ad98c6144b
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2024-12-24 18:26:09 +0900

    [Bronze V] Title: A/B, Time: 172 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a <= 0 || b<= 0){
+            return;
+        }
+        System.out.println((double)a / b);
+    }
+
+    
+}
