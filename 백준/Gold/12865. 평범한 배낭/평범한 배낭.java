e48e6aec6d0324a597e89a8d8d24a593b40505bc
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2025-02-26 09:11:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 15528 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, k, w;
+    static int[] weight, value, dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        weight = new int[n + 1];
+        value = new int[n + 1];
+        dp = new int[k + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            weight[i] = Integer.parseInt(st.nextToken());
+            value[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = k; j >= weight[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
+            }
+        }
+        System.out.println(dp[k]);
+    }
+}
