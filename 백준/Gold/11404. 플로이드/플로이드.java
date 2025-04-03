62851f5c32913bba34d130acf21ffca3fe2491b9
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2025-04-03 09:26:31 +0900

    [Gold IV] Title: 플로이드, Time: 364 ms, Memory: 43440 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -17,8 +17,8 @@ public class Main {
 
         dist = new int[n + 1][n + 1];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
                 if (i == j) {
                     dist[i][j] = 0;
                 } else {
@@ -35,8 +35,7 @@ public class Main {
             int c = Integer.parseInt(st.nextToken());
             dist[a][b] = Math.min(dist[a][b], c);
         }
-
-
+        
         for (int k = 1; k <= n; k++) {
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= n; j++) {
