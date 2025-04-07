07424e8dfd218f51fa5fd1e0f95e138c0cdccec1
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2025-04-07 15:15:24 +0900

    [Silver I] Title: 접두사, Time: 112 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/1141. 접두사/접두사.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+        int N = Integer.parseInt(br.readLine());
+        String[] strings = new String[N];
+ 
+        for (int i = 0; i < N; i++) {
+            strings[i] = br.readLine();
+        }
+ 
+        Arrays.sort(strings, (str1, str2) -> str2.length() - str1.length());
+        List<String> nonPrefixStrings = new ArrayList<>();
+ 
+        for (String currentString : strings) {
+            if (nonPrefixStrings.size() == 0) {
+                nonPrefixStrings.add(currentString);
+                continue;
+            }
+ 
+            boolean isPrefix = false;
+ 
+            for (String existingString : nonPrefixStrings) {
+                if (existingString.indexOf(currentString) == 0) {
+                    isPrefix = true;
+                    break;
+                }
+            }
+ 
+            if (!isPrefix) {
+                nonPrefixStrings.add(currentString);
+            }
+        }
+ 
+        System.out.println(nonPrefixStrings.size());
+    }
+}
