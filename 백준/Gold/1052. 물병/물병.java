ca517934ba83cca425d3233d5d441d0bda5b6133
parkjonggyeong18 <81209336+parkjonggyeong18@users.noreply.github.com>
2025-04-12 14:43:38 +0900

    [Gold V] Title: 물병, Time: 112 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int buy = 0;
+
+        while (Integer.bitCount(n) > k) {
+            n++;
+            buy++;
+        }
+
+        System.out.println(buy);
+    }
+}
